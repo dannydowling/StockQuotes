5df77d3a70ecdd5c13c2300f1cf5cb398f352759
Danny Dowling <danny.dowling@gmail.com>
2021-01-25 04:07:15 -0900

    trying to get it working

--- FetchData.cs
+++ FetchData.cs
@@ -1,34 +1,43 @@
 using Newtonsoft.Json.Linq;
+using StockPivots;
 using System;
 using System.Collections.Generic;
-using System.Dynamic;
-using System.IO;
-using System.Linq;
 using System.Net.Http;
+class WebLookup
+{
 
+    readonly string _quote;
+    readonly HttpClient _client;
+    public List<string> results { get; internal set; }
 
-class WebLookup{
+    readonly List<DateTime> _dates = new List<DateTime>();
+    readonly List<decimal> _opens = new List<decimal>();
+    readonly List<decimal> _highs = new List<decimal>();
+    readonly List<decimal> _lows = new List<decimal>();
+    readonly List<decimal> _closes = new List<decimal>();
+    readonly List<Int32> _volumes = new List<Int32>();
 
-            readonly List<DateTime> _dates = new List<DateTime>();
-            readonly List<decimal> _opens = new List<decimal>();
-            readonly List<decimal> _highs = new List<decimal>();
-            readonly List<decimal> _lows = new List<decimal>();
-            readonly List<decimal> _closes = new List<decimal>();
-            readonly List<Int32> _volumes = new List<Int32>();
+    public WebLookup(string quote, HttpClient client)
+    {
+        _quote = quote;
+        _client = client;
 
-    public void FetchData (string Quote, HttpClient client){
-
-    string APIKey = "GUUNDXU41QUOVFW9";
-      var url = string.Format(
-          "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={0}&apikey={1}", Quote, APIKey);
-            var uri = new Uri(url, UriKind.Absolute);
-            var response = client.GetStringAsync(uri).Result;
-
-           LoadQuotes(response);
-           ParseQuotes();
+        string response = FetchData();
+        LoadQuotes(response);
+        ParseQuotes();
     }
 
-    public void LoadQuotes(string response)
+    private string FetchData()
+    {
+
+        string APIKey = "GUUNDXU41QUOVFW9";
+        var url = string.Format(
+            "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={0}&apikey={1}", _quote, APIKey);
+        var uri = new Uri(url, UriKind.Absolute);
+        return _client.GetStringAsync(uri).Result;
+    }
+
+    private void LoadQuotes(string response)
     {
         Dictionary<string, decimal> quoteInformation = new Dictionary<string, decimal>();
         JObject q = JObject.Parse(response);
@@ -36,13 +45,13 @@ class WebLookup{
         //parse the JSON objects and deconstruct them to the readonly lists of information.
         for (int i = 1; i < q.Count; i++)
         {
-            DateTime date = Convert.ToDateTime(q.SelectToken("properties.0.value"));
+            DateTime date = Convert.ToDateTime(q.SelectToken("Key.value"));
 
-            decimal open = Convert.ToDecimal(q.SelectToken("properties.1.value"));
-            decimal high = Convert.ToDecimal(q.SelectToken("properties.2.value"));
-            decimal low = Convert.ToDecimal(q.SelectToken("properties.3.value"));
-            decimal close = Convert.ToDecimal(q.SelectToken("properties.4.value"));
-            Int32 volume = Convert.ToInt32(q.SelectToken("properties.5.value"));
+            decimal open = Convert.ToDecimal(q.SelectToken("Information.open.value"));
+            decimal high = Convert.ToDecimal(q.SelectToken("Information.high.value"));
+            decimal low = Convert.ToDecimal(q.SelectToken("Information.low.value"));
+            decimal close = Convert.ToDecimal(q.SelectToken("Information.close.value"));
+            Int32 volume = Convert.ToInt32(q.SelectToken("Information.volume.value"));
 
 
             _dates.Add(date);
@@ -51,27 +60,40 @@ class WebLookup{
             _lows.Add(low);
             _closes.Add(close);
             _volumes.Add(volume);
+
         }
-    }    
-    
-public void ParseQuotes(){
-    
-            for (int i = 0; i < _dates.Count - 4; i++)
+    }
+
+    private void ParseQuotes()
+    {
+
+        for (int i = 0; i < _dates.Count - 5; i++)
+        {
+            if (_opens[i] > _highs[i + 1] && _closes[i] < _lows[i + 1])
             {
-                if (_opens[i] > _highs[i + 1] && _closes[i] < _lows[i + 1])
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Pivot downside {0}", _dates[i].ToShortDateString());
-                }
-                if (_opens[i] < _lows[i + 1] && _closes[i] > _highs[i + 1])
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Pivot upside {0}", _dates[i].ToShortDateString());
-                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Pivot downside {0}", _dates[i].ToShortDateString());
+            }
+            if (_opens[i] < _lows[i + 1] && _closes[i] > _highs[i + 1])
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Pivot upside {0}", _dates[i].ToShortDateString());
+            }
+        }
+
+        for (int i = 0; i < _dates.Count - 5; i++)
+        {
+            if (_opens[i] > _highs[i + 1] && _closes[i] < _lows[i + 1])
+            {
+                results.Add(("Pivot downside {0}", _dates[i].ToShortDateString()).ToString());
+            }
+            if (_opens[i] < _lows[i + 1] && _closes[i] > _highs[i + 1])
+            {
+                results.Add(("Pivot upside {0}", _dates[i].ToShortDateString()).ToString());
             }
         }
     }
-        
-        
+}
 
-           
+
+
