cd238ced2860f395c9a219e215d88ef1af3479bc
Danny Dowling <danny.dowling@gmail.com>
2021-01-25 00:24:49 -0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+
+namespace StockPivots
+{  
+class Program
+    {      
+        HttpClient _client {get; set;}
+        string _quote {get; set;}
+            static void Main(string[] args)
+            {                
+                  string _quote = ""; 
+                  HttpClient _client = new HttpClient();
+
+
+            if (args.Length < 1)
+            {
+                _quote = "msft";
+            }
+            else
+            {
+                _quote = args[0].ToString();
+            }
+              
+                   var webLookup = new WebLookup();
+                    webLookup.FetchData(_quote, _client);
+                               
+            }
+        }
+    }     
+
