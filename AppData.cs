374d50cf466aa9e51cd3abfe1c044ad008e996a3
Danny Dowling <danny.dowling@gmail.com>
2021-03-16 07:40:22 -0800

    update...

---
+++ AppData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+
+namespace StockQuotes
+{
+    public class AppData
+    {
+        private ParseData _parseData;
+
+        public List<Quote> AllQuotes { get; private set; }
+        public Dictionary<DateTime, Quote> AllQuotesByKey { get; private set; }
+
+        public LinkedList<Quote> ProfileBuilder { get; } = new LinkedList<Quote>();
+
+        public void Initialize(HttpClient client, List<string> quotes)
+        {
+            
+
+            string APIKey = "GUUNDXU41QUOVFW9";
+
+            string response = "";
+
+            for (int i = 0; i < quotes.Count; i++)
+            {
+                var url = string.Format(
+               "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={0}&apikey={1}", quotes[i], APIKey);
+                var uri = new Uri(url, UriKind.Absolute);
+                response = response + client.GetStringAsync(uri).Result;
+            }        
+           
+
+            using (StreamReader sr = new StreamReader(response))
+            {
+                sr.ReadLine();
+                string JSONdataLine;
+                while ((JSONdataLine = sr.ReadLine()) != null)
+                {
+                    AllQuotes.Add(_parseData.ReadQuoteFromStream(JSONdataLine));
+                }
+            }
+
+            _parseData.ProcessQuotes(AllQuotes);
+        }
+
+    }
+}
---
+++ Quote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockQuotes
+{
+    public class Quote
+    {
+        
+        public Quote(DateTime date, decimal open, decimal high, decimal low, decimal close)
+        {
+            Date = date;
+            Open = open;
+            High = high;
+            Low = low;
+            Close = close;
+        }
+     
+        public DateTime Date { get; }
+        public decimal Open { get; }
+        public decimal High { get; }
+        public decimal Low { get; }
+        public decimal Close { get; }
+    }
+}
