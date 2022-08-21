b8efe9020c35c8dfc742368fdcb54d40200b2927
Danny Dowling <danny.dowling@gmail.com>
2022-08-20 21:20:30 -0800

    Working

--- AppData.cs
+++ AppData.cs
@@ -17,11 +17,11 @@ namespace StockQuotes
             GetQuoteDataFromWeb(APIKey, args);
         }
         public void GetQuoteDataFromWeb(string APIKey, List<string> args)
-        {            
+        {
             string response = "";
             string nameOfStock = "";
             HttpClient client = new HttpClient();
-            StringBuilder sb = new StringBuilder();            
+            StringBuilder sb = new StringBuilder();
             List<StockQuote> quotes = new List<StockQuote>();
 
             for (int i = 0; i < args.Count;)
@@ -39,47 +39,62 @@ namespace StockQuotes
                 nameOfStock = args[i];
                 try
                 {
-                    foreach (JToken item in jsonData.SelectToken("['Time Series (Daily)']"))
-                    {
-                        DataSetByDay dataSet = item.ToObject<DataSetByDay>();
-
-                        quotes = item.Select(x => new StockQuote()
+                    foreach (JToken item in jsonData.SelectTokens("['Time Series (Daily)']"))
+                    {   
+                        foreach (var datedCollection in item.Children().Children())
                         {
-                            Name = nameOfStock,
-                            date = dataSet.date,
-                            open = dataSet.open,
-                            high = dataSet.high,
-                            low = dataSet.low,
-                            close = dataSet.close,
-                            volume = dataSet.volume                  
-                        }).ToList();
+                            sb.Clear();
+                            string[] pathArray = datedCollection.Path.Split('.');
+                            sb.Append(pathArray[1]);
+
+                            DataSetByDay dataSet = datedCollection.ToObject<DataSetByDay>();
+
+                            var quote = datedCollection.Select(x => new StockQuote()
+                            {
+                                Name = nameOfStock,
+                                date = Convert.ToDateTime(sb.ToString()),
+                                open = dataSet.open,
+                                high = dataSet.high,
+                                low = dataSet.low,
+                                close = dataSet.close,
+                                volume = dataSet.volume
+                            });
+
+                            quotes.Add(quote.First());
+
+                            dataSet = null;
+                        }
                     }
+                    compareQuotes(quotes);
                 }
                 catch (Exception err)
                 {
-                    Console.WriteLine(err.Message, err.InnerException);
+                   Console.WriteLine(err.Message, err.InnerException, err.Source);
                     i++;
                 }
-
                 i++;
             }
+        }
+    
 
-            for (int i = 0; i < quotes.Count - 4; i++)
-            {
-                if (quotes[i].open > quotes[i + 1].high && quotes[i].close < quotes[i + 1].low)
+            internal void compareQuotes(List<StockQuote> quotes) {
+                for (int i = 0; i < quotes.Count() - 4; i++)
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("{0}", quotes[i].Name);
-                    Console.WriteLine("Pivot downside {0}", quotes[i].date);
-                }
-                if (quotes[i].open < quotes[i + 1].low && quotes[i].close > quotes[i + 1].high)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("{0}", quotes[i].Name);
-                    Console.WriteLine("Pivot upside {0}", quotes[i].date);
+                    if (quotes[i].open > quotes[i + 1].high && quotes[i].close < quotes[i + 1].low)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("{0}", quotes[i].Name);
+                        Console.WriteLine("Pivot downside {0}", quotes[i].date);
+                    }
+                    if (quotes[i].open < quotes[i + 1].low && quotes[i].close > quotes[i + 1].high)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("{0}", quotes[i].Name);
+                        Console.WriteLine("Pivot upside {0}", quotes[i].date);
+                    }
                 }
             }
         }
     }
-}
+
 
--- Stock_Quote.cs
+++ Stock_Quote.cs
@@ -5,19 +5,25 @@ using System;
 namespace StockQuotes
 {
     // used to deserialize the datetime correctly from the website
-    internal class CustomDateTimeConverter : IsoDateTimeConverter
+    internal class CustomDateTimeConverter : DateTimeConverterBase
     {
-        public CustomDateTimeConverter()
-        { base.DateTimeFormat = "yyyy-mm-dd"; }
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            reader.DateFormatString = "yyyy-mm-dd";
+            reader.ReadAsDateTimeOffset();
+         return serializer.Deserialize<DateTime>(reader);                       
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.DateFormatString = "yyyy-mm-dd";
+            serializer.Serialize(writer, value);
+        }
     }
 
     // each daily record from the website
     internal class DataSetByDay
     {
-        [JsonConverter(typeof(CustomDateTimeConverter))]
-        [JsonProperty("['Time Series (Daily)']")]
-        public DateTime date { get; set; }
-
         [JsonProperty("1. open")]
         public decimal open { get; set; }
 
@@ -37,5 +43,8 @@ namespace StockQuotes
     internal class StockQuote : DataSetByDay
     {
        public string Name { get; set; }
+
+        [JsonConverter(typeof(CustomDateTimeConverter))]
+        public DateTime date { get; set; }
     }
 }
